f358b22065e0e7fd9420f15e4be4fee41243c247
jalabm <jalabm@code.edu.az>
2024-05-11 22:11:55 +0400

    shop page

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', function () {
     var scrollPosition = window.scrollY;
     console.log(header);
     // Change background color and height when scroll position is greater than 100px
-    if (scrollPosition > 300) {
+    if (scrollPosition > 40) {
       header.style.backgroundColor = '#000'; // Change background color to black
       header.style.height = 'height: 80px;'; // Change header height
       headerhome.style.padding = '20px 0px '
